a469838b0dd946faf7d13a3c2cade68bee24704d
Niko Shin <nshin17@gmail.com>
2022-04-18 22:12:32 -0700

    javascript.js readme objective completed.

--- javascript.js
+++ javascript.js
@@ -1,33 +1,45 @@
 
 //variables to hold the available moves:
-let availMoves = ['rock', 'paper', 'scissor'];
+let availMoves = ['r', 'p', 's'];
+
+//variable to count the number of wins for the player:
+let playerWins = 0;
+
+//variable to count computer wins:
+let computerWins = 0;
+
+//variable to count number of ties:
+let tieRound = 0;
 
 
-//variable to hold the random (computer-generated) move:
-//note that the *function declaration* hoists to the top so this variable does NOT run into a 
-//reference error unlike using *function expressions*.
-let computerSelection = computerPlay();
-
-//variable to hold the players move:
-let playerSelection;
+//FUNCTION BELOW:
 
 
-
-
-//function that keeps the game going n number of times:
-function game(playerWins, computerWins) {
+//keep the game going n number of times:
+function game() {
+    
     for (let i = 0; i < 5; i++) {
-        playRound(playerSelection, computerSelection);
+        playRound(playerPlay(), computerPlay());
+        console.log(`Player Wins: ${playerWins} | Computer Wins: ${computerWins} | Tie Games: ${tieRound}`);
     } if (playerWins > computerWins) {
-        alert("Player wins the game!")
+        alert("! PLAYER WINS !")
+    } else if (computerWins > playerWins) {
+        alert("! YOU LOSE !")
     } else {
-        alert("Uh-oh, computer wins the game!")
+        alert("No one wins today...")
     }
+
+}
+
+//get players move:    
+function playerPlay() {
+    let playerSelection = prompt("Choose rock (r), paper (p) or scissor (s)!").toLowerCase();
+    return playerSelection;
 }
 
 
 
-//function that returns 'rock, paper, or scissor':
+//get the computer's random move:
 function computerPlay() {
     let randomMove = availMoves[Math.floor(Math.random()*availMoves.length)];
     return randomMove;
@@ -39,35 +51,30 @@ function computerPlay() {
 
 //function that takes 2 parameters and declares winner of the single round played:
 function playRound(playerSelection, computerSelection) {
-    //get players move:    
-    playerSelection = prompt("Choose rock, paper or scissor!").toLowerCase();
-
-    //variable to count the number of wins for the player:
-    let playerWins = 0;
-    
-    //variable to count computer wins:
-    let computerWins = 0;
-    
-    if ((playerSelection === 'rock' && computerSelection === 'scissor') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissor' && computerSelection === 'paper')) {
-        alert(`You win!
-        You chose ${playerSelection} and computer chose ${computerSelection}`);
-        ++playerWins;
-    } else if (playerSelection === computerSelection) {
-        alert(`It's a tie!
-        You chose ${playerSelection} and computer chose ${computerSelection}`);
+    //tie game:
+    if (playerSelection === computerSelection) {
+        console.log(`It's a tie! You chose ${playerSelection} and computer chose ${computerSelection}`);
+        tieRound = ++tieRound;
+        return tieRound;
+    //win game:
+    } else if ((playerSelection === 'r' && computerSelection === 's') || (playerSelection === 'p' && computerSelection === 'r') || (playerSelection === 's') && computerSelection === 'p') {
+        console.log(`You win!You chose ${playerSelection} and computer chose ${computerSelection}`);
+        playerWins = ++playerWins;
+        //console.log("Player Wins: " + playerWins);
+        return playerWins;
+    //lose game:
     } else {
-        alert(`You lose!
-        You chose ${playerSelection} and computer chose ${computerSelection}`);
-        ++computerWins;
+        console.log(`You lose! You chose ${playerSelection} and computer chose ${computerSelection}`);
+        computerWins = ++computerWins;
+        //console.log("Computer Wins: " + computerWins);
+        return computerWins;
     }
-    //win counter:
-    console.log(`Player Wins: ${playerWins} | Computer Wins ${computerWins}`)
 }
 
-console.log(playRound(playerSelection, computerSelection))
 
 
 
-game()
 
+
+game();
 
