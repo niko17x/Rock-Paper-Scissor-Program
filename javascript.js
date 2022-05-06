ae00fe42a77f6761716e13130800b87db59f7d87
Niko Shin <nshin17@gmail.com>
2022-05-06 09:15:26 -0700

    no changes - just part of prior commit changes

--- javascript.js
+++ javascript.js
@@ -9,92 +9,110 @@ const paperImg = document.getElementById('paper').src='images/paper.png';
 const scissorImg = document.getElementById('scissor').src='images/scissor.png';
 
 
+// text that tells the user if won, lost or tied:
+const text = document.querySelector('.display-text');
 
-// player must be able to click on one of the images and return a value to be able to compare with the bot move.
+// selects the "wins" text:
+const winScore = document.querySelector('#win-score');
+
+// selects the "loss" text:
+const loseScore = document.querySelector('#lose-score');
+
 
 
 //variables to hold the available moves:
 let availMoves = [rockImg, paperImg, scissorImg];
-
-
-
 //variable to count the number of wins for the player:
 let playerWins = 0;
-
 //variable to count computer wins:
 let computerWins = 0;
-
 //variable to count number of ties:
 let tieRound = 0;
+
+
+
 
 
 //FUNCTION BELOW:
 
 
-//keep the game going n number of times:
-function game() {
-    
-    for (let i = 0; i < 5; i++) {
-        playRound(playerPlay(), computerPlay());
-        console.log(`Player Wins: ${playerWins} | Computer Wins: ${computerWins} | Tie Games: ${tieRound}`);
-    } if (playerWins > computerWins) {
-        alert("! PLAYER WINS !")
-    } else if (computerWins > playerWins) {
-        alert("! YOU LOSE !")
-    } else {
-        alert("No one wins today...")
-    }
-console.log("Game over!")
-playAgain();
+function main() {
+    playerChoice();
+    // if (wins === 'game over' || lose === 'game over') {
+    //     playAgain();
+    // }
 
 }
 
-//get players move:    
-// function playerPlay() {
-//     let playerSelection = prompt("Choose rock (r), paper (p) or scissor (s)!").toLowerCase();
-//     if (playerSelection === 'r' || playerSelection === 'p' || playerSelection === 's') {
-//         return playerSelection;
-//     } else {
-//         alert("That is not a valid input. Please try again.");
-//     }
-//     playerPlay();
-// }
+main();
+
+// function that converts r, p, s to rock, paper, scissors for better readability during gameplay:
+function convertLetters(letter) {
+    if (letter === 'r') return 'Rock';
+    if (letter === 'p') return 'Paper';
+    if (letter === 's') return 'Scissor';
+}
+
+// win/loss:
+function wins(userChoice, computerChoice) {
+    playerWins++;
+    winScore.innerHTML = playerWins;
+    text.textContent = convertLetters(userChoice) + ' beats ' + convertLetters(computerChoice) + " - Winner!"
+}
+
+function lose(userChoice, computerChoice) {
+    computerWins++;
+    loseScore.innerHTML = `${computerWins}`;
+    text.textContent = convertLetters(userChoice) + ' loses to ' + convertLetters(computerChoice) + " - Loser!"
+}
+
+function tie(userChoice, computerChoice) {
+    tieRound++;
+    text.textContent = "You both chose " + convertLetters(computerChoice) + " - it's a tie!"
+}
+
+// function that determines win, loss or tie of game:
+function game(userChoice) {
+    const computerChoice = computerPlay();
+    switch(userChoice + computerChoice) {
+        case 'rs':
+        case 'pr':
+        case 'sp':
+            wins(userChoice, computerChoice);
+            break;
+        case 'rp':
+        case 'ps':
+        case 'sr':
+            lose(userChoice, computerChoice);
+            break;
+        case 'rr':
+        case 'pp':
+        case 'ss':
+            tie(userChoice, computerChoice);
+            break;
+    }
+}
+
+
 
 // returns the corresponding letter based on the image clicked:
+// function should ask for player which image to click.
 function playerChoice() {
     rock.addEventListener('click', () => {
-        //console.log('r');
-        return 'r';
-    })
+        game('r');
+    });
     paper.addEventListener('click', () => {
-        //console.log('p');
-        return 'p';
-    })
+        game('p');
+    });
     scissor.addEventListener('click', () => {
-        //console.log('s');
-        return 's';
-    })
+        game('s');
+    });
 }
-
-
-
-//function that compares the return value of the image clicked and the random bot play:
-function playerPlay() {
-    if (playerChoice === 'r' || playerChoice === 'p' || playerChoice === 's') {
-        return playerChoice;
-    } else {
-        console.log("That is not a valid input. Please try again.");
-    }
-    playerPlay(); //keeps running this function until valid input from player.
-}
-
-
-
 
 
 //get the computer's random move:
 function computerPlay() {
-    let randomMove = availMoves[Math.floor(Math.random()*availMoves.length)];
+    let randomMove = availMoves[Math.floor(Math.random()*availMoves.length)]; // returns 0, 1 or 2.
     document.getElementById('bot-img').src = randomMove; //replace filler image in html with random image.
     if (randomMove === rockImg) {
         console.log('r');
@@ -108,32 +126,11 @@ function computerPlay() {
     }
     //return randomMove;
 }
-//console.log(computerPlay())
-computerPlay();
+//computerPlay();
 
 
 
-//function that takes 2 parameters and declares winner of the single round played:
-function playRound(playerSelection, computerSelection) {
-    //tie game:
-    if (playerSelection === computerSelection) {
-        console.log(`It's a tie! You chose ${playerSelection} and computer chose ${computerSelection}`);
-        tieRound = ++tieRound;
-        return tieRound;
-    //win game:
-    } else if ((playerSelection === 'r' && computerSelection === 's') || (playerSelection === 'p' && computerSelection === 'r') || (playerSelection === 's') && computerSelection === 'p') {
-        console.log(`You win!You chose ${playerSelection} and computer chose ${computerSelection}`);
-        playerWins = ++playerWins;
-        //console.log("Player Wins: " + playerWins);
-        return playerWins;
-    //lose game:
-    } else {
-        console.log(`You lose! You chose ${playerSelection} and computer chose ${computerSelection}`);
-        computerWins = ++computerWins;
-        //console.log("Computer Wins: " + computerWins);
-        return computerWins;
-    }
-}
+
 
 //replay game if player types 'y':
 function playAgain() {
@@ -148,7 +145,4 @@ function playAgain() {
     }
 } 
 
-
-
-//game();
 
